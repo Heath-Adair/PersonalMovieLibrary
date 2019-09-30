eb7c2d7ac900f8c043eee8efdecfa7501f461dca
Heath Adair <w.heath.adair@gmail.com>
2019-09-29 21:25:11 -0500

    Extract interface for MovieService

--- src/main/java/com/hadair/personalmovielib/MovieService.java
+++ src/main/java/com/hadair/personalmovielib/MovieService.java
@@ -1,66 +1,17 @@
 package com.hadair.personalmovielib;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class MovieService {
+public interface MovieService {
+    Long addMovie(Movie newMovie);
 
-	private MovieRepository movieRepository;
+    Movie updateMovie(Long movieId, Movie updatedMovie);
 
-	public MovieService(MovieRepository movieRepository) {
-		this.movieRepository = movieRepository;
-	}
+    Movie getMovieByID(Long movieId);
 
-	public Long addMovie(Movie newMovie) {
-		if(!movieRepository.findByTitleIgnoreCaseAndYearReleased(newMovie.getTitle(), newMovie.getYearReleased()).isEmpty()) {
-            //Movie already exists
-		    return -1L;
-		} else {
-			Movie savedMovie = movieRepository.save(newMovie);
-			if (savedMovie == null) {
-				//Failed to save movie
-				return -2L;
-			} else {
-				return savedMovie.getId();
-			}
-		}
-	}
+    int deleteMovie(Long movieId);
 
-	public Movie updateMovie(Long movieId, Movie updatedMovie) {
-		//TODO if new movie field is null do not update that field, but maybe if blank? Maybe handle this as part of validation
-		Movie movie = movieRepository.findOne(movieId);
-		if(movie != null) {
-			movie.setTitle(updatedMovie.getTitle());
-			movie.setDuration(updatedMovie.getDuration());
-			movie.setGenre(updatedMovie.getGenre());
-			movie.setRating(updatedMovie.getRating());
-			movie.setYearReleased(updatedMovie.getYearReleased());
-			movie = movieRepository.save(movie);
-		}
-		return movie;
-	}
+    Iterable<Movie> getMovies();
 
-	public Movie getMovieByID(Long movieId) {
-		return movieRepository.findOne(movieId);
-	}
-
-	public int deleteMovie(Long movieId) {
-		if (!movieRepository.exists(movieId)) {
-			return 0;
-		} else {
-			movieRepository.delete(movieId);
-			return 1;
-		}
-	}
-
-	public Iterable<Movie> getMovies() {
-		return movieRepository.findAll();
-	}
-
-	public List<Movie> getMovieByTitle(String movieTitle) {
-		//TODO - Address issues for movie titles like "The Mummy" vs "Mummy, The"?
-		return movieRepository.findByTitleIgnoreCase(movieTitle.trim());
-	}
+    List<Movie> getMovieByTitle(String movieTitle);
 }
---
+++ src/main/java/com/hadair/personalmovielib/MovieServiceImpl.java
@@ -0,0 +1,72 @@
+package com.hadair.personalmovielib;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+	private MovieRepository movieRepository;
+
+	public MovieServiceImpl(MovieRepository movieRepository) {
+		this.movieRepository = movieRepository;
+	}
+
+	@Override
+	public Long addMovie(Movie newMovie) {
+		if(!movieRepository.findByTitleIgnoreCaseAndYearReleased(newMovie.getTitle(), newMovie.getYearReleased()).isEmpty()) {
+            //Movie already exists
+		    return -1L;
+		} else {
+			Movie savedMovie = movieRepository.save(newMovie);
+			if (savedMovie == null) {
+				//Failed to save movie
+				return -2L;
+			} else {
+				return savedMovie.getId();
+			}
+		}
+	}
+
+	@Override
+	public Movie updateMovie(Long movieId, Movie updatedMovie) {
+		//TODO if new movie field is null do not update that field, but maybe if blank? Maybe handle this as part of validation
+		Movie movie = movieRepository.findOne(movieId);
+		if(movie != null) {
+			movie.setTitle(updatedMovie.getTitle());
+			movie.setDuration(updatedMovie.getDuration());
+			movie.setGenre(updatedMovie.getGenre());
+			movie.setRating(updatedMovie.getRating());
+			movie.setYearReleased(updatedMovie.getYearReleased());
+			movie = movieRepository.save(movie);
+		}
+		return movie;
+	}
+
+	@Override
+	public Movie getMovieByID(Long movieId) {
+		return movieRepository.findOne(movieId);
+	}
+
+	@Override
+	public int deleteMovie(Long movieId) {
+		if (!movieRepository.exists(movieId)) {
+			return 0;
+		} else {
+			movieRepository.delete(movieId);
+			return 1;
+		}
+	}
+
+	@Override
+	public Iterable<Movie> getMovies() {
+		return movieRepository.findAll();
+	}
+
+	@Override
+	public List<Movie> getMovieByTitle(String movieTitle) {
+		//TODO - Address issues for movie titles like "The Mummy" vs "Mummy, The"?
+		return movieRepository.findByTitleIgnoreCase(movieTitle.trim());
+	}
+}
