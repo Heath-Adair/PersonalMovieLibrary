809997f04ea884fa53dcb3c979daf315504772d7
Heath Adair <w.heath.adair@gmail.com>
2021-01-13 00:23:32 -0600

    fix MovieControllerIT tests

--- src/test/java/com/hadair/personalmovielib/MovieControllerIT.java
+++ src/test/java/com/hadair/personalmovielib/MovieControllerIT.java
@@ -1,7 +1,6 @@
 package com.hadair.personalmovielib;
 
 import com.hadair.Application;
-import com.hadair.personalmovielib.Movie;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,19 +16,19 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-//Integration Testing
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//TODO Fix/Rewrite these broken tests
 public class MovieControllerIT {
 
     @LocalServerPort
     private int port;
-    private TestRestTemplate restTemplate = new TestRestTemplate();
-    private HttpHeaders headers = new HttpHeaders();
+    private TestRestTemplate restTemplate;
+    private HttpHeaders headers;
 
     @Before
     public void before() {
+        restTemplate = new TestRestTemplate();
+        headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
     }
 
@@ -40,7 +39,7 @@ public class MovieControllerIT {
     @Test
     public void testGetEmptyMovieList() throws Exception {
         ResponseEntity<List<Movie>> response = restTemplate.exchange(
-                createURLWithPort("/api/movie/list"),
+                createURLWithPort("/api/movies/list"),
                 HttpMethod.GET,
                 new HttpEntity<String>(headers),
                 new ParameterizedTypeReference<List<Movie>>() {});
@@ -48,13 +47,13 @@ public class MovieControllerIT {
     }
 
     @Test
-    public void addQuestion() {
+    public void addMovie() {
         Movie movie = new Movie("Spider-Man: Homecoming", "Action", 2017, "PG-13", "2h 13m");
         HttpEntity<Movie> entity = new HttpEntity<>(movie, headers);
         ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/api/movie/"),
+                createURLWithPort("/api/movies"),
                 HttpMethod.POST, entity, String.class);
         String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
-        assertTrue(actual.contains("/api/movie/"));
+        assertTrue(actual.contains("/api/movies"));
     }
 }
