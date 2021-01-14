fd9e9014e3be5c10f3e15d00a2f3c709b35b1f67
Heath Adair <w.heath.adair@gmail.com>
2021-01-13 22:21:49 -0600

    refactor MovieControllerTest

--- src/test/java/com/hadair/personalmovielib/MovieControllerTest.java
+++ src/test/java/com/hadair/personalmovielib/MovieControllerTest.java
@@ -1,18 +1,14 @@
 package com.hadair.personalmovielib;
 
 import com.hadair.exceptions.ElementNotFoundException;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
@@ -21,29 +17,22 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//JUnit testing with Mockito
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = MovieController.class)
-//TODO refactor
 public class MovieControllerTest {
+
+    private static final String API_MOVIES_URL = "/api/movies";
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private MovieService movieService;
-
     @MockBean
     private MovieRepository movieRepository;
 
-
-//    // ------------------- Return the current local time -----------------
-//    @GetMapping("/api/timeOfDay")
-//    public String getTimeOfDay() {
-//        return movieService.getTimeOfDay();
-//    }
-//
 //    // ------------------- Add a movie by ID -----------------------------
 //    @PostMapping("/api/movie/")
 //    public ResponseEntity<Void> addMovie(@RequestBody Movie newMovie) {
@@ -57,89 +46,96 @@ public class MovieControllerTest {
 //    }
 
     @Test
-    public void testRetrieveDetailsForMovieByID() throws Exception {
+    public void getMovieById_movieExists() throws Exception {
         Movie mockMovie = new Movie(1L,"Spider-Man: Homecoming", "Action", 2017, "PG-13", "2h 13m");
         when(movieService.getMovieByID(anyLong())).thenReturn(mockMovie);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/api/movies/1").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        String expected = "{\"id\":1,\"title\":\"Spider-Man: Homecoming\",\"genre\":\"Action\",\"yearReleased\":2017,\"rating\":\"PG-13\",\"duration\":\"2h 13m\"}";
-
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(API_MOVIES_URL + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.title").value("Spider-Man: Homecoming"))
+                .andExpect(jsonPath("$.genre").value("Action"))
+                .andExpect(jsonPath("$.yearReleased").value("2017"))
+                .andExpect(jsonPath("$.rating").value("PG-13"))
+                .andExpect(jsonPath("$.duration").value("2h 13m"));
     }
 
     @Test
-    public void testRetrieveDetailsForMovieByTitle() throws Exception {
+    public void getMovieByTitle_movieExists() throws Exception {
         Movie mockMovie = new Movie(1L,"Spider-Man: Homecoming", "Action", 2017, "PG-13", "2h 13m");
         List<Movie> mockMovieList = new ArrayList<>();
         mockMovieList.add(mockMovie);
         when(movieService.getMovieByTitle(anyString())).thenReturn(mockMovieList);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/api/movies/search/Spider-Man: Homecoming").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        String expected = "[{\"id\":1,\"title\":\"Spider-Man: Homecoming\",\"genre\":\"Action\",\"yearReleased\":2017,\"rating\":\"PG-13\",\"duration\":\"2h 13m\"}]";
-
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(API_MOVIES_URL + "/search/Spider-Man: Homecoming")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].title").value("Spider-Man: Homecoming"))
+                .andExpect(jsonPath("$[0].genre").value("Action"))
+                .andExpect(jsonPath("$[0].yearReleased").value("2017"))
+                .andExpect(jsonPath("$[0].rating").value("PG-13"))
+                .andExpect(jsonPath("$[0].duration").value("2h 13m"));
     }
 
     @Test
-    public void testDeleteMovieAccepted() throws Exception {
+    public void deleteMovie_movieExists() throws Exception {
         doNothing().when(movieService).deleteMovie(anyLong());
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete(
-                "/api/movies/1").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        //202 Accepted
-        Assert.assertEquals(202, result.getResponse().getStatus());
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete(API_MOVIES_URL + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isAccepted());
     }
 
     @Test
-    public void testDeleteMovieNotFound() throws Exception {
+    public void deleteMovie_movieDoesNotExists() throws Exception {
         doThrow(new ElementNotFoundException("")).when(movieService).deleteMovie(any());
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete(
-                "/api/movies/1").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        //404 Not Found
-        Assert.assertEquals(404, result.getResponse().getStatus());
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete(API_MOVIES_URL + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    public void testGetMovieListMultipleMovies() throws Exception {
+    public void getMovies_multipleMoviesExist() throws Exception {
         List<Movie> mockMovieList = new ArrayList<>();
         mockMovieList.add(new Movie(1L,"Spider-Man: Homecoming", "Action", 2017, "PG-13", "2h 13m"));
         mockMovieList.add(new Movie(2L, "Dr. Strange", "Action", 2016, "PG-13", "1h 55m"));
         when(movieService.getMovies()).thenReturn(mockMovieList);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/api/movies/list").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        String expected = "[{\"id\":1,\"title\":\"Spider-Man: Homecoming\",\"genre\":\"Action\",\"yearReleased\":2017,\"rating\":\"PG-13\",\"duration\":\"2h 13m\"},{\"id\":2,\"title\":\"Dr. Strange\",\"genre\":\"Action\",\"yearReleased\":2016,\"rating\":\"PG-13\",\"duration\":\"1h 55m\"}]";
-
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(API_MOVIES_URL + "/list")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].title").value("Spider-Man: Homecoming"))
+                .andExpect(jsonPath("$[0].genre").value("Action"))
+                .andExpect(jsonPath("$[0].yearReleased").value("2017"))
+                .andExpect(jsonPath("$[0].rating").value("PG-13"))
+                .andExpect(jsonPath("$[0].duration").value("2h 13m"))
+                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[1].title").value("Dr. Strange"))
+                .andExpect(jsonPath("$[1].genre").value("Action"))
+                .andExpect(jsonPath("$[1].yearReleased").value("2016"))
+                .andExpect(jsonPath("$[1].rating").value("PG-13"))
+                .andExpect(jsonPath("$[1].duration").value("1h 55m"));
     }
 
     @Test
-    public void testGetMovieListNoMovies() throws Exception {
+    public void getMovies_noMoviesExist() throws Exception {
         List<Movie> mockMovieList = new ArrayList<>();
         when(movieService.getMovies()).thenReturn(mockMovieList);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/api/movies/list").accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        String expected = "[]";
-
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(API_MOVIES_URL + "/list")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
     }
 }
 
